bb0f381a884dc74fc32e1a44e4b2ba6004768ce8
paul-giraudon <paul.giraudon@gmail.com>
2022-06-03 17:49:03 +0000

    Deploying to gh-pages from master @ 74fb17bfece129cfd6bd40b0e801dbe5a51a0d04 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,79 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
+import { iframeQueryMapTypeGuards } from "@workadventure/iframe-api-typings/Api/Events/IframeEvent";
+import {bootstrapExtra, getLayersMap} from "@workadventure/scripting-api-extra";
 
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    console.log('toto')
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function playUriParse(url: string): {
+    base: string;
+    mode: string;
+    slug: string;
+    map: string;
 }
+{
+    const comp = url.split('/');
+    let cur = 'start';
+    let base = '';
+    let slug = '';
+    let mode = ''
+    let map='';
+    comp.forEach( part =>  {
+        if (cur == 'start') {
+            if (part == '_') {
+                mode = 'public';
+                cur = 'slug';
+            } else if (part=='@') 
+            {
+                mode = 'controlled';
+                cur='slug';
+            } else {
+                base += part + '/';
+            }
+        }
+        else if (cur == 'slug' ) {
+            slug = part ;
+            cur = 'map' ;
+        }
+        else if (cur == 'map' ) {
+            map += part + '/';
+        }
+    });
+   
+    map = map.substring(0, map.length - 1);  
+    if (mode =='public') {
+        map = 'https://' + map;
+    }
+    return {base, mode, slug, map};
+}
+
+
+
+
+WA.onInit().then(async () => {
+    let layers = await getLayersMap();
+    console.log('Current player name: ', WA.player.name);
+    console.log(layers);
+
+    layers.forEach((val,key) => {
+        if (val && val.properties) {
+            val.properties.forEach( prop => {
+                if (prop.type=='string' && prop.name=='goto') {
+                    console.log("GotoLayer : " + key);
+                    WA.room.onEnterLayer(key).subscribe(() => {
+                        if ( prop.value == 'slug') {
+                            const playStr = playUriParse(WA.room.id);
+                            WA.nav.goToPage('https://' + playStr.slug + '/?action=goOAuth&serviceName=myjamespot');
+                        } else {
+                            WA.nav.goToPage(prop.value);
+                        }
+                    })
+                } 
+            });
+        }
+    })
+
+});
+
